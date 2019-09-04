aca79905544e12ad901aa14a9cf3a98df78e01a8
liushibao2017 <32379217+liushibao2017@users.noreply.github.com>
2019-09-04 10:13:55 +0800

    代码提交

--- ExcelAddIn1/ThisAddIn.cs
+++ ExcelAddIn1/ThisAddIn.cs
@@ -6,6 +6,7 @@ using System.Xml.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
+using System.Windows.Forms;
 
 namespace ExcelAddIn1
 {
@@ -13,6 +14,7 @@ namespace ExcelAddIn1
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            MessageBox.Show("VSTO,我来了");
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
